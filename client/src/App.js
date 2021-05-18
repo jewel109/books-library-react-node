2a665db0654b7f532a2c1036887e714e197dfe20
jewel109 <jewel140990@gmail.com>
2021-05-18 12:50:00 +0600

    they are changed

--- client/src/App.js
+++ client/src/App.js
@@ -5,30 +5,14 @@ import {BrowserRouter,Link, Route, Switch} from 'react-router-dom'
 import SignIn from './components/SignIn';
 import Todolist from './components/Todolist';
 import LogOut from './components/LogOut';
+import StartedPage from './pages/StartedPage';
 
 
 function App() {
 	return (
 		<BrowserRouter>
 			<ChakraProvider>
-				<Container
-						maxW='sm'
-						bgColor='pink.100'
-						padding='4'
-						borderRadius='sm'
-				mt='5'>
-					<Header/>
-					<Switch>
-						<Route exact path='/'>
-							<HomePage />
-						</Route>
-						<Route path='/sign-in'>
-							<SignIn />
-						</Route>
-						<Route path="/todolist" component={Todolist} />
-						<Route path="/log-out" component={LogOut} />
-					</Switch>
-				</Container>
+				<StartedPage/>
 			</ChakraProvider>
 		</BrowserRouter>
 	);
